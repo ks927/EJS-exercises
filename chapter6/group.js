91b54d1cd7cc1807733f95da16b66c83c5e5686b
Ksheehan <ksheehan927@gmail.com>
2020-05-12 22:45:26 -0400

    clean up and !== check

--- chapter6/group.js
+++ chapter6/group.js
@@ -5,29 +5,29 @@ class Group {
   
     add(val) {
         if(!this.group.includes(val)) {
-            this.group.push(val)
+            this.group.push(val);
         }
     }
   
     delete(val) {
         let newGroup = this.group.filter(item => {
-            return item != val
+            return item !== val
         });
 
         this.group = newGroup;
     }
   
     has(val) {
-        return this.group.includes(val)
+        return this.group.includes(val);
     }
   
     static from(iterable) {
         var newGroup = new Group();
 
         for (let i in iterable) {
-            newGroup.add(iterable[i])
+            newGroup.add(iterable[i]);
         }
-        
+
         return newGroup;
     }
   }
@@ -36,7 +36,7 @@ class Group {
   // foo.add(10);
   // foo.add(20);
   // foo.has(30);
-  foo.delete(10);
-  foo.add(10);
-  foo.has(10)
+  foo.delete('10');
+//   foo.add(10);
+  foo.has(10) // true because deep val check
   // foo
